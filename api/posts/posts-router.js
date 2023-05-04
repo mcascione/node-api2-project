c1785a900d38e5484ba36b54ddecd3e4600a67bb
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-04 13:37:09 -0700

    comment GET method implemented for MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -37,25 +37,25 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   const { title, contents } = req.body;
-    if (!title || !contents) {
-      res.status(400).json({
-        message: "Please provide title and contents for the post",
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post",
+    });
+  } else {
+    Post.insert({ title, contents })
+      .then(({ id }) => {
+        return Post.findById(id);
+      })
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+        });
       });
-    } else {
-        Post.insert({ title, contents })
-            .then(({id}) => {
-                return Post.findById(id)
-            })
-            .then(post => {
-                res.status(201).json(post);
-            })
-            .catch ((err) => {
-                console.log(err);
-                res.status(500).json({
-                message: "There was an error while saving the post to the database",
-                });
-            });
-    }
+  }
 });
 
 router.put("/:id", (req, res) => {
@@ -105,15 +105,37 @@ router.delete("/:id", (req, res) => {
         });
       } else {
         res.status(200).json(post);
-        return Post.remove(id)
+        return Post.remove(id);
       }
     })
     .catch((err) => {
-      console.log(err.message);
+      console.log(err);
       res.status(500).json({
         message: "The post could not be removed",
       });
     });
 });
 
+router.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  Post.findById(id)
+    .then((post) => {
+      if (!post) {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      } else {
+        return Post.findPostComments(id);
+      }
+    })
+    .then((comments) => {
+      res.status(200).json(comments);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "The comments information could not be retrieved",
+      });
+    });
+});
 module.exports = router;
