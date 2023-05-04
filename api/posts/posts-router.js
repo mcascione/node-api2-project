9744aeab5a72a1ba4164e56a4738def4b6986a66
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-04 10:22:13 -0700

    implemented GET Method for endpoint /api/posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,6 +3,17 @@ const express = require('express');
 const Post = require('./posts-model');
 const router = express.Router();
 
-
+router.get('/', (req, res) => {
+    Post.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "The posts information could not be retrieved"
+            })
+        })
+})
 
 module.exports = router;
